0ed24c8d20d1c33cf8fc20a327ad31e6b87c050e
paulbkim1 <110564809+paulbkim1@users.noreply.github.com>
2022-09-14 08:10:15 -0700

    Delete belt_exam directory

--- belt_exam/flask_app/controllers/2_controller.py
+++
@@ -1,5 +0,0 @@
-from crypt import methods
-from flask_app import app
-from flask import render_template, request, session, redirect, flash
-from flask_app.models.users_model import Users
-from flask_app.models.recipes_model import Recipes
